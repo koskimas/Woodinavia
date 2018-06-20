dcbc1fc66ef8ac9d31f8f1caf9420c9231bc8a19
Olli Salli <olli@jakso.me>
2018-06-20 21:03:40 +0300

    Show customer photo if it exists

--- src/InfoPanel.vue
+++ src/InfoPanel.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    {{baeName}}
+    <div class="customer-info"
+         v-bind:style="customerImageBackgroundStyle">
+    </div>
   </div>
 </template>
+
+<style lang="text/scss" scoped>
+  .customer-info {
+    width: 100%;
+    height: 100%;
+
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+</style>
 
 <script>
 
@@ -10,8 +23,14 @@ export default {
   name: 'InfoPanel',
 
   computed: {
-    baeName() {
-      return this.chosenPlot && JSON.stringify(this.chosenPlot.properties)
+    customerImageBackgroundStyle() {
+      if (this.chosenPlot.properties.gx_media_links) {
+        return {
+          backgroundImage: `url('${this.chosenPlot.properties.gx_media_links}')`
+        };
+      } else {
+        return {};
+      }
     }
   },
 
