60df3903d7b5341da5ba57f0c0fdbc1512815fbc
Sami Koskimäki <koskomi@gmail.com>
2018-06-20 13:10:42 +0300

    show plots and forests on map

--- src/PlotMap.vue
+++ src/PlotMap.vue
@@ -9,13 +9,21 @@ export default {
     geoJson: {
       type: Object,
       default: null
+    },
+
+    selectedPlot: {
+      type: Object,
+      default: null
+    },
+
+    selectedPlotChanged: {
+      type: Function,
+      default: () => {}
     }
   },
 
   watch: {
     geoJson(geoJson) {
-      console.log('adjasdjsakasdlkjsajdjsa')
-
       if (this.map && geoJson) {
         this.redraw();
       }
@@ -23,18 +31,21 @@ export default {
   },
 
   created() {
-    // Do not add this to data so that vue doesn't wrap it.
-    this.map = null
+    // Do not add these to data so that vue doesn't wrap them.
+    this.google = null;
+    this.mapItems = [];
+    this.map = null;
   },
 
   mounted() {
     window.woodinavia.googleMapsPromise.then(google => {
+      this.google = google;
+
       this.map = new google.maps.Map(this.$refs.plotMap, {
-        zoom: 4,
-        center: {lat: -25.363, lng: 131.044}
+        zoom: 4
       });
 
-      console.log('map created', this.geoJson)
+      this.map.setMapTypeId('satellite');
 
       if (this.geoJson) {
         this.redraw();
@@ -44,10 +55,49 @@ export default {
 
   methods: {
     redraw() {
-      const forest = this.geoJson.features.find(it => it.geometry.type === 'Polygon')
-      const plots = this.geoJson.features.filter(it => it.geometry.type === 'Point')
+      const google = this.google;
+      const forest = this.geoJson.features.find(it => it.geometry.type === 'Polygon');
+      const plots = this.geoJson.features.filter(it => it.geometry.type === 'Point');
+      const bounds = new google.maps.LatLngBounds();
 
-      console.log(forest, plots)
+      this.mapItems.forEach(item => item.setMap(null))
+      this.mapItems = []
+
+      const forestCoords = forest.geometry.coordinates[0].map(coord => ({
+        lat: coord[1],
+        lng: coord[0]
+      }));
+
+      forestCoords.forEach(coord => {
+        bounds.extend(coord)
+      })
+
+      const forestPoly = new google.maps.Polygon({
+        paths: forestCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+      });
+
+      forestPoly.setMap(this.map)
+      this.mapItems.push(forestPoly)
+
+      plots.forEach(plot => {
+        const marker = new google.maps.Marker({
+          position: {
+            lng: plot.geometry.coordinates[0],
+            lat: plot.geometry.coordinates[1]
+          },
+          map: this.map
+        });
+
+        bounds.extend(marker.getPosition())
+        this.mapItems.push(marker)
+      });
+
+      this.map.fitBounds(bounds);
     }
   }
 }
