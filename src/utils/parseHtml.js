12834cd2830c5d71d49373ccefaa2a6f8654c0f5
Sami Koskimäki <koskomi@gmail.com>
2018-06-26 19:56:17 +0300

    don't download all images always

--- src/utils/parseHtml.js
+++ src/utils/parseHtml.js
@@ -3,6 +3,7 @@ export default function parseTextFromHtml(html) {
     return null;
   }
 
+  html = html.replace(/<img[^>]+>/g, '');
   let wrapperDiv = document.createElement('div');
   wrapperDiv.innerHTML = html;
   return wrapperDiv.innerText.trim();
